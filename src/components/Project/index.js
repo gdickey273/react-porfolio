f36445cb6808bb14566480985b14abebe309c5d4
gdickey273 <gdickey273@gmail.com>
2020-11-12 17:25:38 -0500

    created Project component

---
+++ src/components/Project/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+function Project(props) {
+  return (
+    <a className="project" href={props.deployedLink}>
+      <div className="card">
+        <img className="card-img" src={props.src} alt={`${props.title} Application`} />
+        <div className="card-img-overlay px-0">
+          <div className="c-title">
+            <h5 className="card-title">{props.title}</h5>
+          </div>
+        </div>
+      </div>
+    </a>
+  );
+}
+
+export default Project;
