fb842a1df56a56f6c804e7dee319824b94e379c2
gdickey273 <gdickey273@gmail.com>
2020-11-12 13:09:56 -0500

    Added new Navbar/SideDrawer setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 
+
+import Navbar from "./components/Navbar";
+import SideDrawer from "./components/SideDrawer";
+import Backdrop from "./components/Backdrop";
 function App() {
+  const [sideDrawerOpen, setSideDrawerOpen] = useState(false);
+
+  function drawerToggleClickHandler() {
+    setSideDrawerOpen(!sideDrawerOpen);
+  };
+
+  function backdropClickHandler() {
+    setSideDrawerOpen(false);
+  }
   return (
-    <>
+    <div style={{ height: "100%" }}>
       <Router>
         <div>
-          <Nav />
+          <Navbar drawerToggleClickHandler={drawerToggleClickHandler}/>
+          <SideDrawer show={sideDrawerOpen}/>
+          {sideDrawerOpen &&
+              <Backdrop click={backdropClickHandler}/>
+          }
         </div>
       </Router>
-    </>
+    </div>
   );
 }
 
