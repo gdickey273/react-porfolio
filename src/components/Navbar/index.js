785a0697ddc0a8c8a469ce5f188ad68335bacde8
gdickey273 <gdickey273@gmail.com>
2020-11-12 13:06:35 -0500

    rethought the Navbar in a major way

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,51 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
-function Navbar() {
-  return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light p-0">
-        <Link className="navbar-brand" to="/">Graham Dickey</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+import DrawerToggleButton from "../DrawerToggleButton";
 
-        <div className="collapse navbar-collapse justify-content-end mr-5" id="navbarNavAltMarkup">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link
-                className="nav-link p-2"
-                to="/"
-                >
-                  About
-                </Link>
+
+function Navbar(props) {
+
+  return (
+    <header className = "toolbar">
+      <nav className="toolbar-nav">
+        <div className="toolbar-toggle-button">
+          <DrawerToggleButton click={props.drawerToggleClickHandler}/>
+        </div>
+        <div></div>
+        <Link className="p-3 toolbar-logo" to="/">Graham Dickey</Link>
+        <div className="spacer"></div>
+        <div className="toolbar-nav-items mr-5">
+          <ul>
+            <li>
+              <Link to="/">About</Link>
             </li>
-            <li className="nav-item">
-              <Link
-                className="nav-link p-2"
-                to="/portfolio"
-                >
-                  Portfolio
-                </Link>
+            <li>
+              <Link to="/portfolio">Portfolio</Link>
             </li>
-            <li className="nav-item">
-              <Link
-                className="nav-link p-2"
-                to="/contact"
-                >
-                  Contact
-                </Link>
+            <li>
+              <Link to="/contact">Contact</Link>
             </li>
-            <li className="nav-item">
-              <a
-              className="nav-link p-2"
-              href="assets/resume.pdf" download>Resume</a>
+            <li>
+              <a href="assets/resume.pdf" download>Resume</a>
             </li>
           </ul>
         </div>
       </nav>
-    </>
+    </header>
   )
 }
 
