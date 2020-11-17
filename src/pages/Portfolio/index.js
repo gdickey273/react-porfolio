e21a4ad14afe397860aed8ca2aa2a427af32fc1b
gdickey273 <gdickey273@gmail.com>
2020-11-16 20:54:44 -0500

    fixed whats-in-the-pantry img

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css";
 import Project from "../../components/Project";
+import pantryIMG from "./images/whats-in-the-pantry.png";
 
 function Portfolio(props) {
   return (
@@ -24,31 +25,31 @@ function Portfolio(props) {
 
           <div className="row mt-2 mt-xs-0">
             <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 mt-xs-2">
-              <Project deployedLink="https://gdickey273.github.io/whats-in-the-pantry/"src="assets/images/whats-in-the-pantry.png" title="What's In The Pantry" />
+              <Project deployedLink="https://gdickey273.github.io/whats-in-the-pantry/"src={pantryIMG} title="What's In The Pantry" />
             </div>
 
             <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 mt-xs-2">
-              <Project deployedLink="https://project-2-team-7.herokuapp.com/"src="assets/images/coffeehut.png" title="CoffeeHut Remote Ordering System" />
-            </div>
-          </div>
-
-          <div className="row mt-2 mt-xs-0">
-            <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 mt-xs-2">
-              <Project deployedLink="https://calm-mountain-13174.herokuapp.com/"src="assets/images/burger-screenshot.png" title="Eat-Da-Burger" />
-            </div>
-
-            <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 mt-xs-2">
-              <Project deployedLink="https://gdickey273.github.io/code-quiz/"src="assets/images/code-quiz.png" title="Code Quiz" />
+              <Project deployedLink="https://project-2-team-7.herokuapp.com/"src="./assets/images/coffeehut.png" title="CoffeeHut Remote Ordering System" />
             </div>
           </div>
 
           <div className="row mt-2 mt-xs-0">
             <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 mt-xs-2">
-              <Project deployedLink="https://gdickey273.github.io/weather-dashboard/" src="assets/images/weather-dashboard.png" title="Weather Dashboard" />
+              <Project deployedLink="https://calm-mountain-13174.herokuapp.com/"src="./assets/images/burger-screenshot.png" title="Eat-Da-Burger" />
             </div>
 
             <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 mt-xs-2">
-              <Project deployedLink="https://gdickey273.github.io/work-day-scheduler/" src="assets/images/work-day-scheduler.png" title="Workday Scheduler" />
+              <Project deployedLink="https://gdickey273.github.io/code-quiz/"src="./assets/images/code-quiz.png" title="Code Quiz" />
+            </div>
+          </div>
+
+          <div className="row mt-2 mt-xs-0">
+            <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 mt-xs-2">
+              <Project deployedLink="https://gdickey273.github.io/weather-dashboard/" src="./assets/images/weather-dashboard.png" title="Weather Dashboard" />
+            </div>
+
+            <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 mt-xs-2">
+              <Project deployedLink="https://gdickey273.github.io/work-day-scheduler/" src="./assets/images/work-day-scheduler.png" title="Workday Scheduler" />
             </div>
           </div>
 
